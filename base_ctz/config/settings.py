709a50a6a5a4aab2b44fd2a1764bc04d41f5cabc
Sun Howwrongbum <sun@libermatic.com>
2020-02-06 23:06:53 +0530

    feat: create settings

---
+++ base_ctz/config/settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "label": _("Settings"),
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "Libermatic Settings",
+                    "label": _("Libermatic Settings"),
+                    "description": _("Run setup scripts"),
+                    "settings": 1,
+                },
+            ],
+        },
+    ]
